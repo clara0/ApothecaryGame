6b45264534460f3e7e73e1f72753ca50d31c7754
Clara Fang <clara.fang@gmail.com>
2025-01-27 20:26:31 -0500

    add basic save functionality for the player's position

---
+++ save_files/player_data.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name player_data
+
+# To be saved...
+@export var position : Vector2
+
+func update_position(pos : Vector2):
+	position = pos
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -15,6 +15,26 @@ var _curr_anim: Anim = Anim.IDLE
 var direction: Vector2 = Vector2.ZERO
 @export var speed: int = 100
 
+var save_file_path = "user://save/"
+var save_file_name = "player_save.tres"
+var data = player_data.new()
+
+func _ready() -> void:
+	verify_save_dir(save_file_path)
+	
+func verify_save_dir(path : String):
+	DirAccess.make_dir_absolute(path)
+	
+func load_data() -> void:
+	data = ResourceLoader.load(save_file_path + save_file_name).duplicate(true)
+	self.position = data.position
+	_animation_player.play("RESET")
+	print("Loaded successfully!")
+	
+func save() -> void:
+	ResourceSaver.save(data, save_file_path + save_file_name)
+	print("Saved successfully!")
+
 # runs once per frame
 func _process(_delta):
 	direction = Input.get_vector("left", "right", "up", "down").normalized()
@@ -31,6 +51,12 @@ func _process(_delta):
 		_curr_anim = Anim.RIGHT if x > 0 else Anim.LEFT
 		
 	_animation_player.play(_anim_states[_curr_anim]) 
+	data.update_position(self.position)
+	
+	if Input.is_action_just_pressed("save"):
+		save()
+	elif Input.is_action_just_pressed("load"):
+		load_data()
 	
 # runs 60 times each second
 func _physics_process(_delta):
