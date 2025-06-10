5987eaa9c7350dd77ac27916aabf0e0b35a7eb54
Clara Fang <clara.fang@gmail.com>
2025-06-10 15:53:31 -0400

    inventory ui now updates from player's inventory

--- scenes/inventory/player_inv.gd
+++ scenes/inventory/player_inv.gd
@@ -1,9 +1,21 @@
 extends Control
 
+@onready var inv = preload("res://inventory/mat_inv_player.tres")
+@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+	
 var is_open: bool = false
 
 func _ready() -> void:
+	print("ready")
+	update_slots()
 	close()
+	
+	
+func update_slots()	 -> void:
+	var num_items: int = inv.inv_items.size()
+	var num_slots: int = slots.size()
+	for i in range(min(num_items, num_slots)):
+		slots[i].update(inv.inv_items[i])
 	
 	
 func _process(delta) -> void:
---
+++ scenes/inventory/slot.gd
@@ -0,0 +1,13 @@
+extends Control
+
+@onready var item_img: Sprite2D = $Control/Item
+@onready var item_quant: Label = $Quantity
+
+func update(item: InvItem) -> void:
+	if !item:
+		item_img.visible = false
+	else:
+		item_img.visible = true
+		item_img.texture = item.texture
+		item_quant.text = str(item.quant)
+	
